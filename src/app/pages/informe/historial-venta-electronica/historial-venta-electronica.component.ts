caf7a75f0fe80ad18dc79ce01f553d9548146e8b
Yeimer <ing_yeimer@hotmail.com>
2026-02-03 22:05:09 -0500

    Primer commit

--- src/app/pages/informe/historial-venta-electronica/historial-venta-electronica.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-historial-venta',
-  templateUrl: './historial-venta-electronica.component.html',
-})
-export class HistorialVentaElectronicaComponent {
-  
-}
--- src/app/pages/informe/nomina-electronica/nomina-electronica.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-nomina-electronica',
-  templateUrl: './nomina-electronica.component.html',
-})
-export class NominaElectronicaComponent {
-  
-}
